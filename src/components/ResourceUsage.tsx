8b16fadbfb1a83a0c4dc2b306f7ff951119915bb
Nico <nico.enking@gmail.com>
2023-01-31 14:06:59 +0100

    Prefill resources to prevent layout clipping

--- src/components/ResourceUsage.tsx
+++ src/components/ResourceUsage.tsx
@@ -25,45 +25,41 @@ export default function ResourceUsage() {
     }
   }, [socket]);
 
-  if (currentUsage === null) {
-    return <></>;
-  }
-
   return (
     <Grid w="10rem" mt={1}>
       <Grid.Col span={4}>
         <Center>
           <Flex direction="column" align="center" gap={6}>
-            <Tooltip label={`CPU ${currentUsage.cpu}%`}>
+            <Tooltip label={`CPU ${currentUsage?.cpu || 0}%`}>
               <ThemeIcon color="gray">
                 <IconCpu size={18} />
               </ThemeIcon>
             </Tooltip>
-            <Progress value={currentUsage.cpu} w="100%"/>
+            <Progress value={currentUsage?.cpu || 0} w="100%" mt={2} mb={8}/>
           </Flex>
         </Center>
       </Grid.Col>
       <Grid.Col span={4}>
         <Center>
           <Flex direction="column" align="center" gap={6}>
-            <Tooltip label={`MEM ${currentUsage.mem}%`}>
+            <Tooltip label={`MEM ${currentUsage?.mem || 0}%`}>
               <ThemeIcon color="gray">
                 <IconDatabase size={18} />
               </ThemeIcon>
             </Tooltip>
-            <Progress value={currentUsage.mem} w="100%"/>
+            <Progress value={currentUsage?.mem || 0} w="100%" mt={2} mb={8}/>
           </Flex>
         </Center>
       </Grid.Col>
       <Grid.Col span={4}>
         <Center>
           <Flex direction="column" align="center">
-            <Tooltip label={`${currentUsage.downloads} Downloads`}>
+            <Tooltip label={`${currentUsage?.downloads || 0} Downloads`}>
               <ThemeIcon color="gray">
                 <IconDownload size={18} />
               </ThemeIcon>
             </Tooltip>
-            <Text fz="xs" color="dimmed">{currentUsage.downloads}</Text>
+            <Text fz="xs" color="dimmed" mt={2}>{currentUsage?.downloads || ''}</Text>
           </Flex>
         </Center>
       </Grid.Col>
